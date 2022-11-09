bc7726ae10da89e17ecadcc222f57a16467868e7
TaehyeonJeon <taehyun0540@gmail.com>
2022-11-09 20:07:15 +0900

    use Jquery

--- DarkMode/darkMode.js
+++ DarkMode/darkMode.js
@@ -1,17 +1,14 @@
 var Linker = {
     setColor:function(color){
-        let alist = document.querySelectorAll('a');
-        for(let i in alist){
-            alist[i].style.color = color;
-        }
+        $('a').css('color',color);
     }
 }
 var Body = {
     setColor:function(color){
-        document.querySelector('body').style.color = color;
+        $('body').css('color',color);
     },
     setBackgroundColor:function(color){
-    document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor',color);
     }
 }
 function nightDayHandler(self){
